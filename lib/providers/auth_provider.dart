5dacd5694e8bd36afd6f8b638e2c47bd5efa9812
josepfranquesa <josepfranquesa@bisual.net>
2025-05-24 18:32:07 +0200

    app-release.apk + fixing

--- lib/providers/auth_provider.dart
+++ lib/providers/auth_provider.dart
@@ -1,5 +1,3 @@
-// ignore_for_file: constant_identifier_names
-
 import 'package:SaveIt/domain/login_response.dart';
 import 'package:SaveIt/domain/token.dart';
 import 'package:SaveIt/domain/user.dart';
